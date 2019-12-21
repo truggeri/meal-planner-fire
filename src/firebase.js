2cb3d50ab6fbbdb875b3365729c300f002d64869
Thomas Ruggeri <truggeri@gmail.com>
2019-12-21 17:53:23 -0500

    Adding firebase config js

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import "firebase/analytics";
+import 'firebase/auth';
+import 'firebase/firestore';
+var firebaseConfig = {
+    apiKey: "AIzaSyCV9tF0dLP0xwv0OshBtF8TLc6E7Lwu7Y0",
+    authDomain: "meal-planner-1336c.firebaseapp.com",
+    databaseURL: "https://meal-planner-1336c.firebaseio.com",
+    projectId: "meal-planner-1336c",
+    storageBucket: "meal-planner-1336c.appspot.com",
+    messagingSenderId: "237619101623",
+    appId: "1:237619101623:web:8be149e03421a900ecf232",
+    measurementId: "G-4FB606BBNZ"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const auth = firebase.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+export const db = firebase.firestore();
